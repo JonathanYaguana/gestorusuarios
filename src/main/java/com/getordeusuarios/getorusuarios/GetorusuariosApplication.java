9ea9519330d15d96a419a3035aa75ff3ae2eaa59
JonathanYaguana <jonathansacrox93@gmail.com>
2023-06-30 13:56:05 -0500

    Commit inicial

---
+++ src/main/java/com/getordeusuarios/getorusuarios/GetorusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.getordeusuarios.getorusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetorusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetorusuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getordeusuarios/getorusuarios/GetorusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getordeusuarios.getorusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetorusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
