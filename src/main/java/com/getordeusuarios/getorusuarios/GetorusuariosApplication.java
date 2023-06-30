2b126c6ad7f8f4f96e85cc9f78fe8d5a903c8f53
JonathanYaguana <jonathansacrox93@gmail.com>
2023-06-30 16:06:03 -0500

    funcionamiento del controller

--- src/main/java/com/getordeusuarios/getorusuarios/GetorusuariosApplication.java
+++ src/main/java/com/getordeusuarios/getorusuarios/GetorusuariosApplication.java
@@ -2,8 +2,10 @@ package com.getordeusuarios.getorusuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.getordeusuarios.getorusuarios.controllers"})
 public class GetorusuariosApplication {
 
 	public static void main(String[] args) {
