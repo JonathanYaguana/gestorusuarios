6e6fa077633639249b1215811e963b2229642c71
JonathanYaguana <jonathansacrox93@gmail.com>
2023-07-17 21:28:00 -0500

    Add funcion eliminar

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -19,16 +19,37 @@ async function cargarUsuarios() {
 		let listadoHtml = '';
 		
 		for(let usuario of usuarios){
+
+			let botonHtml ='<a class="btn btn-outline-warning" onclick="eliminarUsuario(' + usuario.id + ')" href="#" role="button">Eliminar</a>';
+
 			let usuarioHtml = 
-			'<tr><td>'+usuario.id+
-			'</td><td>'+usuario.nombre+
-			'</td><td>'+usuario.apellido+
-			'</td><td>'+usuario.email+
-			'</td><td>'+usuario.telefono
-			+'</td><td><a class="btn btn-outline-warning" href="#" role="button">Eliminar</a></td></tr>';
+			'<tr><td>' + usuario.id +
+			'</td><td>' + usuario.nombre +
+			'</td><td>' + usuario.apellido +
+			'</td><td>' + usuario.email +
+			'</td><td>' + usuario.telefono +
+			'</td><td>' + botonHtml + '</td></tr>';
 			
 			listadoHtml += usuarioHtml;	
 		}		
 		
 		document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
+}
+
+async function eliminarUsuario(id){
+	
+	if(!confirm('¿Desea eliminar ese usario?')){
+		return;
+	}
+	
+	const request = await fetch('api/usuarios/' + id, {
+			method: 'DELETE',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		});
+		
+		location.reload()
 }
